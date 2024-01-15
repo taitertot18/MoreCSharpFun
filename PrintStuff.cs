55253375e72f78c44954da81d0a5380e71296395
lilytait <lilytait@LILYTAIT32B6>
2024-01-15 11:09:57 -0700

    Add project files.

---
+++ PrintStuff.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreCSharpFun
+{
+    internal class PrintStuff
+    {
+        private string language;
+
+        /*Contructor - we can have more than one constructor!*/
+
+        public PrintStuff()
+        {
+            language = "EN";
+        }
+        public PrintStuff(string temp)
+        {
+            language = temp;
+        }
+
+        /*the method signature (parameters) need to be different but the
+         * names can be the same*/
+        public void PrintName(string n)
+        {
+            if (language == "EN")
+            {
+                System.Console.WriteLine("Hello, " + n + "!");
+            }
+
+            if (language == "PT")
+            {
+                System.Console.WriteLine("Oi, " + n + "!");
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using MoreCSharpFun;
+using System.Reflection.Metadata.Ecma335;
+using System.Xml.Linq;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        PrintStuff ps = new PrintStuff();
+
+        /*putting name without an = means its null which is not good...*/
+        string name = "";
+        
+        System.Console.WriteLine("Please enter your name");
+        name = System.Console.ReadLine();
+
+        ps.PrintName(name);
+    }
+
+}
